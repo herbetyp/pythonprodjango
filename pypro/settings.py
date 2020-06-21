40563df8ec1da231d01a8c776199bc87bf571114
Pbezerra-dev <herbetyp@gmail.com>
2020-06-21 20:30:36 -0300

    User model custom

--- pypro/settings.py
+++ pypro/settings.py
@@ -116,6 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# Custom User
+AUTH_USER_MODEL = 'core.User'
 
 # Development
 STATIC_URL = '/static/'
