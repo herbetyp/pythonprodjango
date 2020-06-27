6fa7861dda26325e640bb855b6a0c99bba903d6e
Pbezerra-dev <herbetyp@gmail.com>
2020-06-27 19:56:36 -0300

    testando title de home

--- pypro/core/tests/test_home.py
+++ pypro/core/tests/test_home.py
@@ -1,6 +1,12 @@
+from pypro.django_assertions import assert_contains
 from django.test import Client
 
 
 def test_status_code(client: Client):
     response = client.get('/')
     assert response.status_code == 200
+
+
+def test_title(client: Client):
+    response = client.get('/')
+    assert_contains(response, '<title>Python Pro</title>')
