d3018e8b314319d1a8e9059bbea853fa78f87cb1
Pbezerra-dev <herbetyp@gmail.com>
2020-07-05 12:28:50 -0300

    add video for slug

--- pypro/aperitivos/tests/test_page_video.py
+++ pypro/aperitivos/tests/test_page_video.py
@@ -1,20 +1,43 @@
 import pytest
 from django.urls import reverse
+
 from pypro.django_assertions import assert_contains
 
 
 @pytest.fixture
-def resp(client):
-    return client.get(reverse('aperitivos:video', args=('motivacao',)))
+def resp_motivacao(client):
+    return client.get(reverse('aperitivos:video', args=['motivacao']))
 
 
-def test_status_code(resp):
-    assert resp.status_code == 200
+@pytest.fixture
+def resp_instalacao_windows(client):
+    return client.get(reverse('aperitivos:video', args=['instalacao-windows']))
 
 
-def test_title_video(resp):
-    assert_contains(resp, '<h1>Vídeo Aperitivo: Motivação</h1>')
+def test_status_code_motivacao(resp_motivacao):
+    assert resp_motivacao.status_code == 200
 
 
-def test_content_video(resp):
-    assert_contains(resp, '<iframe src="https://player.vimeo.com/video/434538400"')
+def test_title_video_motivacao(resp_motivacao):
+    assert_contains(resp_motivacao, '<h1>Vídeo Aperitivo: Motivação</h1>')
+
+
+def test_content_video_motivacao(resp_motivacao):
+    assert_contains(
+        resp_motivacao, '<iframe src="https://player.vimeo.com/video/434538400"'
+    )
+
+
+def test_status_code_instalacao_windows(resp_instalacao_windows):
+    assert resp_instalacao_windows.status_code == 200
+
+
+def test_title_video_instalacao_windows(resp_instalacao_windows):
+    assert_contains(resp_instalacao_windows, '<h1>Instalação no Windows</h1>')
+
+
+def test_content_video_instalacao_windows(resp_instalacao_windows):
+    assert_contains(
+        resp_instalacao_windows,
+        '<iframe src="https://player.vimeo.com/video/435485443"',
+    )
--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 434538400}
+    videos = {
+        'motivacao': {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 434538400},
+        'instalacao-windows': {
+            'titulo': 'Instalação no Windows',
+            'vimeo_id': 435485443,
+        },
+    }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
