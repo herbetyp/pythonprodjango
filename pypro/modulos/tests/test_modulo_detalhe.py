06715824e745f1138fcaa1248ba25b5a7ba97883
Pbezerra-dev <herbetyp@gmail.com>
2020-08-02 21:05:54 -0300

    add aulas module

--- pypro/modulos/tests/test_modulo_detalhe.py
+++ pypro/modulos/tests/test_modulo_detalhe.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from model_bakery import baker
 
 from pypro.django_assertions import assert_contains
+from pypro.modulos.models import Aula
 from pypro.modulos.models import Modulo
 
 
@@ -12,7 +13,12 @@ def modulo(db):
 
 
 @pytest.fixture
-def response(client, modulo):
+def aulas(modulo):
+    return baker.make(Aula, 3, modulo=modulo)
+
+
+@pytest.fixture
+def response(client, modulo, aulas):
     response = client.get(reverse('modulos:detalhe', kwargs={'slug': modulo.slug}))
     return response
 
@@ -27,3 +33,8 @@ def test_descricao(response, modulo: Modulo):
 
 def test_publico(response, modulo: Modulo):
     assert_contains(response, modulo.publico)
+
+
+def test_aulas_titulos(response, aulas):
+    for aula in aulas:
+        assert_contains(response, aula.titulo)
