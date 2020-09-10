4533413b99dc0cfc72f05aba76ee50cb098c9e1c
Pbezerra-dev <herbetyp@gmail.com>
2020-09-09 22:23:41 -0300

    corrections

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from pypro.modulos.models import Aula
 from pypro.modulos.models import Modulo
 
 
@@ -16,3 +17,7 @@ def encontrar_modulo(slug: str) -> Modulo:
 
 def listar_aulas_de_modulo_ordenadas(modulo: Modulo):
     return list(modulo.aula_set.order_by('order').all())
+
+
+def encontrar_aula(slug):
+    return Aula.objects.get(slug=slug)
