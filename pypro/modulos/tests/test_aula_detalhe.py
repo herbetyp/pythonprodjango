207bc367d81642d319d510d75658cbe56710fee7
Pbezerra-dev <herbetyp@gmail.com>
2020-09-19 21:37:52 -0300

    add breadcrumb in aula screen

--- pypro/modulos/tests/test_aula_detalhe.py
+++ pypro/modulos/tests/test_aula_detalhe.py
@@ -29,3 +29,9 @@ def test_titulo(response, aula: Aula):
 
 def test_vimeo(response, aula: Aula):
     assert_contains(response, f'src="https://player.vimeo.com/video/{ aula.vimeo_id }"')
+
+
+def test_modulo_breadcrumb(response, modulo: Modulo):
+    assert_contains(
+        response, f'<li class="breadcrumb-item"><a href="{modulo.get_absolute_url()}">{ modulo.titulo }</a></li>',
+    )
