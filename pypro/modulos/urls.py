23320823bb255cebd36fdd5ee560defd86a4d9d3
Pbezerra-dev <herbetyp@gmail.com>
2020-09-20 22:17:23 -0300

    start dev add indice

--- pypro/modulos/urls.py
+++ pypro/modulos/urls.py
@@ -5,6 +5,7 @@ from pypro.modulos import views
 app_name = 'modulos'
 
 urlpatterns = [
+    path('', views.indice, name='indice'),
     path('<slug:slug>', views.detalhes, name='detalhe'),
     path('aulas/<slug:slug>', views.aula, name='aula'),
 ]
