92b885559d1c20dc7ca06c95140ca1fe0e5c0986
Pbezerra-dev <herbetyp@gmail.com>
2020-09-23 20:58:24 -0300

    add enroll from the admin

--- pypro/turmas/admin.py
+++ pypro/turmas/admin.py
@@ -3,8 +3,17 @@ from django.contrib import admin
 from pypro.turmas import models
 
 
+class MatriculaInline(admin.TabularInline):
+    model = models.Turma.alunos.through
+    extra = 1
+    readonly_fields = ['data']
+    autocomplete_fields = ['usuario']
+    ordering = ['-data']
+
+
 @admin.register(models.Turma)
 class TurmaAdmin(admin.ModelAdmin):
+    inlines = [MatriculaInline]
     list_display = ['nome', 'slug', 'inicio', 'fim']
     prepopulated_fields = {'slug': ('nome',)}
     ordering = ['-inicio']
