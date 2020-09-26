5c9f2bb9628f5ef8558c4d43a21e4c184c186cc7
Pbezerra-dev <herbetyp@gmail.com>
2020-09-25 21:50:14 -0300

    btn login only user not is autenthicated

---
+++ pypro/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from model_bakery import baker
+
+
+@pytest.fixture
+def user_logged(db, django_user_model):
+    user_model = baker.make(django_user_model)
+
+    return user_model
+
+
+@pytest.fixture
+def client_with_logged_in_user(user_logged, client):
+    client.force_login(user_logged)
+    return client
