b059e4bfe63b883e537ebcb972e47b9e407bac7a
Pbezerra-dev <herbetyp@gmail.com>
2020-09-27 21:03:41 -0300

    add access aula only user logged

--- pypro/modulos/tests/test_aula_detalhe.py
+++ pypro/modulos/tests/test_aula_detalhe.py
@@ -18,20 +18,31 @@ def aula(modulo):
 
 
 @pytest.fixture
-def response(client, aula):
+def resp(client_with_logged_in_user, aula):
+    response = client_with_logged_in_user.get(reverse('modulos:aula', kwargs={'slug': aula.slug}))
+    return response
+
+
+@pytest.fixture
+def resp_not_user(client, aula):
     response = client.get(reverse('modulos:aula', kwargs={'slug': aula.slug}))
     return response
 
 
-def test_titulo(response, aula: Aula):
-    assert_contains(response, aula.titulo)
+def test_titulo(resp, aula: Aula):
+    assert_contains(resp, aula.titulo)
 
 
-def test_vimeo(response, aula: Aula):
-    assert_contains(response, f'src="https://player.vimeo.com/video/{ aula.vimeo_id }"')
+def test_vimeo(resp, aula: Aula):
+    assert_contains(resp, f'src="https://player.vimeo.com/video/{ aula.vimeo_id }"')
 
 
-def test_modulo_breadcrumb(response, modulo: Modulo):
+def test_modulo_breadcrumb(resp, modulo: Modulo):
     assert_contains(
-        response, f'<li class="breadcrumb-item"><a href="{modulo.get_absolute_url()}">{ modulo.titulo }</a></li>',
+        resp, f'<li class="breadcrumb-item"><a href="{modulo.get_absolute_url()}">{ modulo.titulo }</a></li>',
     )
+
+
+def test_user_not_logged_redirect(resp_not_user):
+    assert resp_not_user.status_code == 302
+    assert resp_not_user.url.startswith(reverse('login'))
